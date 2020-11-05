58523c9bbbf2bef9d67e7edfb21f95669d433c98
Mathias De Weerdt <mathias.de.weerdt@gmail.com>
2020-11-05 14:08:46 +0100

    Added example

---
+++ example/src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import Home from '../views/Home.vue';
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/callback',
+        name: 'Callback',
+        component: () =>
+            import(/* webpackChunkName: "about" */ '../views/Callback.vue'),
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
+
+export default router;
