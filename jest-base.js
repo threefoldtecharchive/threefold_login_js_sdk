2ee3fa9e0a64fe664093a804eb5b3bdbca17610f
LennertDefauw <lennert.defauw@jimber.org>
2022-07-18 16:24:02 +0200

    Testing

--- jest-base.js
+++ jest-base.js
@@ -1,12 +1,21 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-    globals: {
-        'ts-jest': {
-            tsconfig: 'tsconfig.spec.json',
-        },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json',
+
     },
-    transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    testEnvironment: 'node',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest"
+
+  },
+  testEnvironment: 'node',
+  "transformIgnorePatterns": [
+    "node_modules/(?!@jimber/.*)",
+
+
+  ]
+
 };
